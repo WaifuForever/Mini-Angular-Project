dc4e81f8fa7c7c5965a35890772df446fd5b7dc2
Waifu_Forever <itachiut1r4@gmail.com>
2021-05-11 00:45:26 -0300

    Add products done

--- src/app/adicionar/adicionar.component.ts
+++ src/app/adicionar/adicionar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { ProdutoService } from '../services/produtos.service'
+import { Produto } from '../models/produto'
 
 @Component({
   selector: 'app-adicionar',
@@ -9,8 +11,9 @@ import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 export class AdicionarComponent implements OnInit {
 
   productForm: FormGroup;
-  
-  constructor(private formBuilder: FormBuilder) { }
+  p1: Produto = null;
+
+  constructor(private formBuilder: FormBuilder, private produtoService: ProdutoService) { }
 
   ngOnInit(): void {
     this.productForm = this.formBuilder.group({
@@ -20,7 +23,21 @@ export class AdicionarComponent implements OnInit {
     });
   }
 
-  addProduto(form: NgForm){
-    console.log(form);
+  addProduto(form: FormGroup){
+
+    this.productForm.controls['nome'].value
+    
+    console.log(form.value);
+    console.log(this.productForm.controls['nome'].value)    
+    console.log(form.valid)
+    this.p1 = new Produto(
+      6,
+      this.productForm.controls['nome'].value,
+      this.productForm.controls['preco'].value,
+      this.productForm.controls['desc'].value,
+      new Date()
+    ) 
+    this.produtoService.cadastrarProduto(this.p1)
+
   }
 }
